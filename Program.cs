37f94a53fcbed1011858e531b45aa772054a0554
UMSF-K15-2-Rogozhkin <rogozhkin0798@gmail.com>
2017-04-06 20:11:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace лаб1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму вклада = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите процент по вкладу = ");
+            double b = double.Parse(Console.ReadLine());
+            double c = a + (a * (b / 100));
+            Console.WriteLine("Через год сумма на вкладе {0:c}", c);
+            //Console.ReadLine();
+            Console.ReadKey(); 
+        }
+    }
+}
